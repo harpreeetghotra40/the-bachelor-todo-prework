2fa39bb2ba2aea46b3e9fd68b3ec14fcfa800414
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-05-19 21:36:28 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -21,11 +21,22 @@ def get_contestant_name(data, occupation)
       end
     end
   end
-  # code here
-end	end
+end
+end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  x = 0
+  data.each do |season , array|
+    array.each do |people|
+      people.each do |k , v|
+        if v == hometown
+          x += 1
+      end
+    end
+  end
+end
+return x
 end
 
 def get_occupation(data, hometown)
