39a049d572949fc8e1c6c94ebc546cebc9026369
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-05-19 21:33:41 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,26 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].each do |women|
+
+        women.each do |k,v|
+         if v == "Winner"
+             name = women["name"]
+             arr = name.split(" ")
+             return arr[0]
+         end
+       end
+     end
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |women|
+    women.each do |k , v|
+      if v == occupation
+        return women["name"]
+    end
+  end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
